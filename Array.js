9ba363330fff727b51de8bfbe9e55ae08c8deee6
Narayan Sharma <narayan_sharma@Narayans-MacBook-Air.local>
2025-11-12 21:36:56 +0530

    sliding window

--- Array.js
+++ Array.js
@@ -67,3 +67,84 @@ var maxProfit = function(prices) {
 };
 console.log("1: ", maxProfit([7,1,5,3,6,4]))
 console.log("2: ", maxProfit([7,6,4,3,1]))
+
+// . Longest Substring Without Repeating Characters
+var lengthOfLongestSubstring = function(s) {
+    let end = 0;
+    let start = 0;
+    let maxLength = 0;
+    const longestString = new Set()
+    while(end < s.length){
+        if(!longestString.has(s[end])){
+            longestString.add(s[end]);
+            end++;
+            maxLength = Math.max(maxLength, longestString.size)
+        }else{
+            longestString.delete(s[start])
+            start++;
+        }
+    }
+    return maxLength
+};
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+// Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+var containsNearbyDuplicate = function(nums, k) {
+    let newSet = new Set()
+    for(let i = 0; i < nums.length; i++){
+        if(newSet.has(nums[i])) return true
+        newSet.add(nums[i])
+        if(newSet.size > k){
+            newSet.delete(nums[i-k])
+        }
+    }
+    return false
+};
+// Example 1:
+
+// Input: nums = [1,2,3,1], k = 3
+// Output: true
+// Example 2:
+
+// Input: nums = [1,0,1,1], k = 1
+// Output: true
+// Example 3:
+
+// Input: nums = [1,2,3,1,2,3], k = 2
+// Output: false
+
+
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+// 3438. Find Valid Pair of Adjacent Digits in String
+var findValidPair = function(s) {
+    let freq = {}
+    for(let i of s){
+        freq[i] = (freq[i] || 0 ) + 1
+    }
+    for(let i = 0; i < s.length-1; i++){
+        let a = s[i]
+        let b = s[i+1]
+        if(parseInt(a) === freq[a] && parseInt(b) === freq[b] && a !== b){
+            return a+b
+        }
+    }
+    return ""
+};
+
+// Input: s = "2523533"
+// Output: "23"
+
+// Input: s = "221"
+// Output: "21"
+
+// Input: s = "22"
+// Output: ""
+
